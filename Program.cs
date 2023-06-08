5ac6f0e1eb1383d4e7771b0ca01a585fbc691b4f
lakido <lakido551@gmail.com>
2023-06-08 20:51:48 +0300

    step_2: array input

--- Program.cs
+++ Program.cs
@@ -2,4 +2,14 @@
 int m = Convert.ToInt32(Console.ReadLine());
 string [] stringArray = new string [m];
 
+//Ввод данных в массив
+void array(string [] stringArray)
+{
+  for (int i = 0;i<stringArray.Length;i++)
+  {
+     Console.WriteLine($"Введите {i+1} элемент массива");
+     stringArray[i] = Console.ReadLine();
+  }
+}
+
 
